73e89622c9ecdda935b00ef814c2bbf7d8c07bc4
huycvip <124803986+huycvip@users.noreply.github.com>
2024-03-05 21:50:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Nói cô gái ấy nghe nè",
+    introTitle: 'Hà Anh ơi',
+    introDesc: `Trái đất này có 7 tỉ người
+    Gặp được một người dễ thương như cậu quả là khó ghê
+    Tớ nói thiệt là tớ khum muốn đánh mất cậu đâu
+    Năm ngoái vì INOVATION CAMP tớ đã đánh mất cơ hội vào HSV
+    Năm nay không thể vì INNOVATION CAMP mà tớ đánh mất người của HSV đâu`,
+    btnIntro: 'Tin tớ nhé!!',
+    title: 'Gỡ nút thắt giữa hai chúng ta đi nào 🥰',
+    desc: 'Chúng ta sẽ viết tiếp những câu chuyện hề hước còn dang dở',
+    btnYes: 'Chịu lun <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Bạn thấy tớ như thế nào',
+    btnReply: 'Tớ thấy cậu cũng quá là cute',
+    reply: 'Chịu thì chịu hông chịu thì chịu',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Gòi gỡ block cho tớ đi nhóe!!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
